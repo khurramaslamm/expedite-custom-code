7a51fb02368d5b11abd6d82fcd4d612d64a528eb
Khurram Aslam <khurram.aslamm@gmail.com>
2021-12-14 15:00:41 +0500

    on scroll effect ( first-half )

--- source.js
+++ source.js
@@ -1,1 +1,18 @@
-console.log("yes");
+$(function() {
+
+    $(document).on('scroll', function() {
+        var top = $(document).scrollTop();
+        console.log(top);
+
+        if (top > 700 && top < 1100) {
+            $('.text-column').addClass('fix-text-column');
+        } else if(top>1100){
+            $('.text-column').addClass('fix-text-column-end');
+            $('.text-column').removeClass('fix-text-column');
+        }
+        else if(top > 1050 && top < 1499){
+            $('.text-column').removeClass('fix-text-column-end');
+        }
+    });
+
+});
