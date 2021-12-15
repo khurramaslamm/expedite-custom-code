1ee64f053177bdaa2eb392a9df5a4867dc9bb305
Khurram Aslam <khurram.aslamm@gmail.com>
2021-12-15 15:22:54 +0500

    on scroll fix div is created

--- source.js
+++ source.js
@@ -1,18 +1,24 @@
 $(function() {
-
     $(document).on('scroll', function() {
         var top = $(document).scrollTop();
         console.log(top);
 
-        if (top > 700 && top < 1100) {
-            $('.text-column').addClass('fix-text-column');
+        if (top > 700 && top < 1151 || top < 1151 && top > 700  ) {
+            $('.text-column').addClass('sticky-top');
         } else if(top>1100){
-            $('.text-column').addClass('fix-text-column-end');
             $('.text-column').removeClass('fix-text-column');
         }
-        else if(top > 1050 && top < 1499){
-            $('.text-column').removeClass('fix-text-column-end');
-        }
     });
+
+    //     var lastScrollTop = 0;
+    // $(window).scroll(function(event){
+    //    var st = $(this).scrollTop();
+    //    if (st > lastScrollTop){
+    //        console.log(st);
+    //    } else {
+    //       console.log(st);
+    //    }
+    //    lastScrollTop = st;
+    // });
 
 });
